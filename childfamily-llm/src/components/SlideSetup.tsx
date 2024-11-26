f61a589b509ebe39a65e41f29d3092e702a41138
Louisa Gracia Diego <lgadiego001@outlook.com>
2024-11-26 21:36:29 +0800

    em

--- childfamily-llm/src/components/SlideSetup.tsx
+++ childfamily-llm/src/components/SlideSetup.tsx
@@ -12,7 +12,7 @@ const SlideSetup = () => {
                         <li>Local setup for privacy and security</li>
                         <li>Consumer grade server: i7 CPU, 32GB RAM, 1 NVidia RTX 3060 GPU</li>
                         <li>LLama3 8B: 8 billion parameters, released April 2024</li>
-                        <li><emph>instruct</emph> model</li>
+                        <li><em>instruct</em> model</li>
                         <li>upgrade to Llama 3.2</li>
                     </ul>
                 </div>
